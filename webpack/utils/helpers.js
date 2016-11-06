fde5e22d319ff868e4d9fadb6c26a93b87bf1aa6
zhaotoday <729234283@qq.com>
2016-11-06 22:42:55 +0800

    update

--- webpack/utils/helpers.js
+++ webpack/utils/helpers.js
@@ -3,10 +3,10 @@ const consts = require('./consts')
 
 const getEntry = (pages) => {
   const entry = {}
-  const items = ['public', ...pages]
+  const allPages = ['public', ...pages]
 
-  items.map((item) => {
-    entry[item] = `./${consts.SRC}${consts.SCRIPTS}${item}.js`
+  allPages.map((page) => {
+    entry[page] = `./${consts.SRC}${consts.SCRIPTS}${page}.js`
   })
 
   return entry
