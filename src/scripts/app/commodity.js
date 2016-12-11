bea2b40ec7b9345d6c9ff418fc7ad61daf06ff9e
zhaotoday <729234283@qq.com>
2016-12-11 21:58:26 +0800

    update

--- src/scripts/app/commodity.js
+++ src/scripts/app/commodity.js
@@ -1,1 +1,13 @@
 require('styles/app/commodity/index.scss')
+require('../$/plugins/slider')
+
+$('#commodity-images').slider({
+  contentCls: 'slider-images',
+  prevBtnCls: 'slider-left',
+  nextBtnCls: 'slider-right',
+  disableBtnCls: 'disabled',
+  triggerType: 'click',
+  easing: 'easeOut',
+  inEndEffect: 'none',
+  auto: true
+})
