4e77354f94d9fa105ef93aaa3e01cf2cb6daf2d0
zhaotoday <729234283@qq.com>
2016-12-11 19:28:21 +0800

    update

--- src/scripts/app/home.js
+++ src/scripts/app/home.js
@@ -1,14 +1,24 @@
 require('styles/app/home/index.scss')
 require('../$/plugins/slider')
+
 $('#ad').slider({
   contentCls: 'slider-images',
   navCls: 'slider-indexes',
-  animate: true,
-  triggerType: 'click',
   auto: true,
   delay: 2500,
   duration: 700,
   keyboardAble: true,
   inEndEffect: 'cycle',
   easing: 'easeOut'
-});
+})
+
+$('#commodity').slider({
+  contentCls: 'slider-images',
+  prevBtnCls: 'slider-left',
+  nextBtnCls: 'slider-right',
+  disableBtnCls: 'disabled',
+  triggerType: 'click',
+  easing: 'easeOut',
+  inEndEffect: 'none',
+  steps: 3
+})
