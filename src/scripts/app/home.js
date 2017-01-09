8ac4caa974c45a8364e8a3720ae0bc5db3b7965b
820514 <820514@nd>
2017-01-09 15:00:06 +0800

    update

--- src/scripts/app/home.js
+++ src/scripts/app/home.js
@@ -1,1 +1,11 @@
 require('themes/app/home/slider/index.scss')
+require('assets/zepto/plugins/slider')
+
+$('#home-slider').slider({
+  contentCls: 'slider-images',
+  inEndEffect: 'switch',
+  fullScreen: false,
+  easing: 'easeIn',
+  navCls: 'slider-indexes'
+})
+
