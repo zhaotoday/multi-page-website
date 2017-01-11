2bcbcc0cf257eee66728332c2357de1abe2fd70b
zhaotoday <729234283@qq.com>
2017-01-11 23:38:12 +0800

    update

--- src/scripts/app/home.js
+++ src/scripts/app/home.js
@@ -1,6 +1,7 @@
 require('themes/app/home/slider/index.scss')
 require('themes/app/home/about/index.scss')
 require('themes/app/home/commodities/index.scss')
+require('themes/app/home/contact/index.scss')
 require('assets/zepto/plugins/slider')
 
 $('#home-slider').slider({
@@ -11,3 +12,12 @@ $('#home-slider').slider({
   navCls: 'slider-indexes'
 })
 
+$('#home-commodities').slider({
+  contentCls: 'slider-images',
+  inEndEffect: 'none',
+  fullScreen: false,
+  easing: 'easeIn',
+  prevBtnCls: 'slider-left',
+  nextBtnCls: 'slider-right',
+  disableBtnCls: 'disabled'
+})
