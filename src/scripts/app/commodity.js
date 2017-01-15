81fe4f21213ab533ec59c92cdf6ebf5e6389aaf9
zhaotoday <729234283@qq.com>
2017-01-16 00:18:19 +0800

    update

--- src/scripts/app/commodity.js
+++ src/scripts/app/commodity.js
@@ -1,1 +1,3 @@
-require('themes/app/commodity/index.scss')
+require('themes/app/commodity/details/index.scss')
+require('themes/app/commodity/slider/index.scss')
+require('themes/app/commodity/more/index.scss')
