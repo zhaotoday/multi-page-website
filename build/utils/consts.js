4727e0d0f7fbf64df65adc88f35e5cdccda438b1
zhaotoday <729234283@qq.com>
2017-01-23 00:00:38 +0800

    update

--- build/utils/consts.js
+++ build/utils/consts.js
@@ -2,7 +2,7 @@ const SRC = 'src/'
 const DIST = 'dist/'
 const SCRIPTS = 'scripts/app/'
 const STYLES = 'themes/app/'
-const PAGES = ['home', 'commodity']
+const PAGES = ['home', 'commodity', 'jobs']
 const CDN = '/'
 
 module.exports = {
