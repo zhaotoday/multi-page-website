a620e4da8e8dffbe0e279445e96414f038e33067
zhaotoday <729234283@qq.com>
2017-01-22 22:45:36 +0800

    update

--- src/scripts/app/commons.js
+++ src/scripts/app/commons.js
@@ -1,3 +1,3 @@
-require('themes/app/commons/global.scss')
+require('themes/app/commons/global/index.scss')
 require('themes/app/commons/header/index.scss')
 require('themes/app/commons/footer/index.scss')
