36e6c66194c99b83d1bd26cdc8d75e2ce516914d
zhaotoday <729234283@qq.com>
2017-02-06 23:37:57 +0800

    update

--- src/scripts/app/commons.js
+++ src/scripts/app/commons.js
@@ -1,3 +1,12 @@
 require('themes/app/commons/global/index.scss')
 require('themes/app/commons/header/index.scss')
 require('themes/app/commons/footer/index.scss')
+require('../utils/zepto')
+
+$('#collapse').on('tap', function () {
+  $('#nav-overlay').show().removeClass('fadeOutRight').addClass('fadeInRight')
+})
+
+$('#nav-close').on('tap', function () {
+  $('#nav-overlay').show().removeClass('fadeInRight').addClass('fadeOutRight')
+})
